45a2d8f53627819564fb53d8c046396a694df8e1
ajitsingh25 <ajitsingh25@gmail.com>
2020-07-04 18:07:39 +0000

    adding comments

--- HackerRank/arr_maniplation.py
+++ HackerRank/arr_maniplation.py
@@ -2,15 +2,21 @@ def arr_man(n, q):
     
     arr = [0] * (n)
     for a, b, k in q:
+      # add k in the list
         arr[a-1] += k
         if b+1 <= n:
+          # Substract the k from the list to keep the list balance
+          # so that while adding list element we can get the max sum
             arr[b] -= k
         # print(arr)
             
     max_sum = 0
+
+    #sum of all alements in the  list
+    #max_sum to assign max sum while adding all elements in the list
     s = 0
     for i in arr:
-        s += i
+        s += i    
         max_sum = max(max_sum, s)
         
     print(max_sum)
